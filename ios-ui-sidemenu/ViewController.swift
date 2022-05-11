92930e13188110cd44d944f85cd1b60cb0fa5fdc
jmkim <wjdals0304@gmail.com>
2022-05-11 14:37:35 +0900

    Initial Commit

---
+++ ios-ui-sidemenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-ui-sidemenu
+//
+//  Created by 김정민 on 2022/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
