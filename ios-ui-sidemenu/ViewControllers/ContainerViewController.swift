c8626050a8f18f659f148c66f47d4e34a8182b41
jmkim <wjdals0304@gmail.com>
2022-05-14 17:15:18 +0900

    add: menu 선택시 이벤트 처리

--- ios-ui-sidemenu/ViewControllers/ContainerViewController.swift
+++ ios-ui-sidemenu/ViewControllers/ContainerViewController.swift
@@ -19,6 +19,8 @@ final class ContainerViewController: ViewController {
     
     private let menuVC = MenuViewController()
     private let homeVC = HomeViewController()
+    private var navVC: UINavigationController?
+    lazy var infoVC = InfoViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,7 @@ final class ContainerViewController: ViewController {
     
     private func addChildVCs() {
         // Menu
+        menuVC.delegate = self
         addChild(menuVC)
         view.addSubview(menuVC.view)
         menuVC.didMove(toParent: self)
@@ -38,29 +41,84 @@ final class ContainerViewController: ViewController {
         addChild(navVC)
         view.addSubview(navVC.view)
         navVC.didMove(toParent: self)
+        self.navVC = navVC
     }
 }
 
 extension ContainerViewController: HomeViewControllerDelegate {
-    
     func didTapMenuButton() {
+        print(#function)
+        toggleMenu(completion: nil)
+    }
+    
+    func toggleMenu(completion: (() -> Void)?) {
         // Animate the menu
         switch menuState {
         case .closed:
+            // open it
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
                 
-                self.homeVC.view.frame.origin.x = self.homeVC.view.frame.size.width - 100
+                self.navVC?.view.frame.origin.x = self.homeVC.view.frame.size.width - 100
                 
-                
-            } completion: { done in
+            } completion: { [weak self] done in
                 if done {
-                    
+                    self?.menuState = .opened
                 }
             }
 
         case .opened:
-            break
+            //close it
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
+                self.navVC?.view.frame.origin.x = 0
+            } completion: { [weak self] done in
+                if done {
+                    self?.menuState = .closed
+                    
+                    DispatchQueue.main.async {
+                        completion?()
+                    }
+
+                }
+            }
+
         }
     }
+}
+
+extension ContainerViewController: MenuViewControllerDelegate {
+
+    func didSelect(menuItem: MenuViewController.MenuOptions) {
+        print("did select")
+        toggleMenu { [weak self] in
+            switch menuItem {
+            case .home:
+                self?.resetToHome()
+            case .info :
+                self?.addInfo()
+            case .appRating:
+                break
+            case .shareApp:
+                break
+            case .settings :
+                break
+          }
+        }
+    }
+    
+    func addInfo() {
+        let vc = infoVC
+        homeVC.addChild(vc)
+        homeVC.view.addSubview(vc.view)
+        vc.view.frame = view.frame
+        vc.didMove(toParent: homeVC)
+        homeVC.title = vc.title
+    }
+    
+    func resetToHome() {
+        infoVC.view.removeFromSuperview()
+        infoVC.didMove(toParent: nil)
+        homeVC.title = "Home"
+    }
+    
     
 }
